49225677fe014f21db99892cfddfa3aab470f781
GithubJjang <rs60504@naver.com>
2023-11-24 11:49:28 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/demo300/Demo300Application.kt
@@ -0,0 +1,11 @@
+package com.example.demo300
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Demo300Application
+
+fun main(args: Array<String>) {
+	runApplication<Demo300Application>(*args)
+}
