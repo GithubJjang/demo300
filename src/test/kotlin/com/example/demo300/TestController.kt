a5c4f9a9da200d2b72773ea32256a027cdf2ec13
GithubJjang <rs60504@naver.com>
2023-12-01 14:05:56 +0900

    success test

--- src/test/kotlin/com/example/demo300/TestController.kt
+++ src/test/kotlin/com/example/demo300/TestController.kt
@@ -2,20 +2,22 @@ package com.example.demo300
 
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-@RestController
-class TestController {
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+
+@WebMvcTest(TestController::class)
+class TestControllerTest {
 
     @Autowired
-    private lateinit var mockMvc: MockMvc;
+    lateinit var mockMvc: MockMvc
 
     @Test
-    fun helloTest(){
+    fun testHelloEndpoint() {
         mockMvc.perform(get("/"))
-            .andExpect(status().isOk);
+            .andExpect(status().isOk)
+            .andExpect(content().string("hello123"))
     }
 }
