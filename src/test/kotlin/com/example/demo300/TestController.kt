5d8c3500665bc3b444d806810bb170a14c6c38f2
GithubJjang <rs60504@naver.com>
2023-12-03 21:20:08 +0900

    not using githooks

--- src/test/kotlin/com/example/demo300/TestController.kt
+++ src/test/kotlin/com/example/demo300/TestController.kt
@@ -18,7 +18,7 @@ class TestControllerTest {
     fun testHelloEndpoint() {
         mockMvc.perform(get("/"))
             .andExpect(status().isOk)
-            .andExpect(content().string("abcdefghik"))
+            .andExpect(content().string("hello123"))
 
     }
 }
