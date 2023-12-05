3c5aa93e2476e09b52ce0491d08c273b46ebfcca
GithubJjang <rs60504@naver.com>
2023-12-05 15:45:18 +0900

    success.develop

--- src/test/kotlin/com/example/demo300/TestController.kt
+++ src/test/kotlin/com/example/demo300/TestController.kt
@@ -18,7 +18,7 @@ class TestControllerTest {
     fun testHelloEndpoint() {
         mockMvc.perform(get("/"))
             .andExpect(status().isOk)
-            .andExpect(content().string("hello1243"))
+            .andExpect(content().string("hello123"))
 
     }
 }
