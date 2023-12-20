46b7684cfb814ae91cd2613dc3f2107c2a430e2a
GithubJjang <rs60504@naver.com>
2023-12-20 16:59:54 +0900

    test1

--- src/main/kotlin/com/example/demo300/dummyController/TestController.kt
+++ src/main/kotlin/com/example/demo300/dummyController/TestController.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+//
 /*
 @RestController
 class TestController {
